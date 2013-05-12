e5af273c6e8c5f5f3b90d655a25943bdcb332282
Sarah McRoberts <mcrosa01@luther.edu>
2013-05-12 17:46:58 -0500

    got our first graph working + new examples

--- models/x_fixtures.py
+++ models/x_fixtures.py
@@ -1,27 +1,27 @@
 ## coding: utf8
-#import datetime
-#import yaml
-#import os
+import datetime
+import yaml
+import os
 
 #cwd = os.getcwd()
 #filepath = os.path.join(cwd, "applications/weathertrendz/private/data.yaml")
 
 #if db(db.Readings.id > 0).count() == 0:
-    #data = yaml.load(open(filepath, 'r'))
-    #keys = data.keys()
-    #keys.sort()
-    #for key in keys:
-        #high = int(data[key]['max'])
-        #low = int(data[key]['min'])
-        #precip = int(data[key]['pre'])
-        #snow = int(data[key]['sno'])
-        #db.Readings.insert(
-            #recorded_on=datetime.datetime.strptime(key, "%Y-%m-%d").date(),
-            #hightemp=high if high != -99 and high != -9900 else None,
-            #lowtemp=low if low != -99 and low != -9900 else None,
-            #precipitation=precip if precip != -99 and precip != -9900 else None,
-            #snowfall=snow if snow != -99 and snow != -9900 else None,
-       #)
+#    data = yaml.load(open(filepath, 'r'))
+#    keys = data.keys()
+#    keys.sort()
+#    for key in keys:
+#        high = int(data[key]['max'])
+#        low = int(data[key]['min'])
+#        precip = int(data[key]['pre'])
+#        snow = int(data[key]['sno'])
+#        db.Readings.insert(
+#            recorded_on=datetime.datetime.strptime(key, "%Y-%m-%d").date(),
+#            hightemp=high if high != -99 and high != -9900 else None,
+#            lowtemp=low if low != -99 and low != -9900 else None,
+#            precipitation=precip if precip != -99 and precip != -9900 else None,
+#            snowfall=snow if snow != -99 and snow != -9900 else None,
+#       )
 
 
 #filepath = os.path.join(cwd, "applications/weathertrendz/private/marcus-data.yaml")
