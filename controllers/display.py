b012f8e7d61ad7de7b3f5715a8c0929914e855b6
Sarah McRoberts <mcrosa01@luther.edu>
2013-05-14 21:40:39 -0500

    adjustable dates for everyone

--- controllers/display.py
+++ controllers/display.py
@@ -5,18 +5,18 @@ def base():
     return {}
 
 def highsAndLows():
-    rows = getReadingsInRange(db, date(2012,9,1), date(2012,9,30))
+    rows = getReadingsInRange(db, date(2012,9,1), date(2012,9,30), 'recorded_on', 'hightemp', 'lowtemp')
     return dict(rows=rows.as_list())
 
 def trendz():
     rows = getDayReadingInRange(db, date(2012,9,5), 15)
 
 def precipitation():
-    rows = getReadingsInRange(db,date(2000,9,1), date(2012,9,30))
+    rows = getReadingsInRange(db,date(2000,9,1), date(2012,9,30), 'recorded_on', 'precipitation')
     return dict(rows=rows.as_list())
 
 def snowfall():
-    rows = getReadingsInRange(db,date(2000,9,1), date(2012,9,30))
+    rows = getReadingsInRange(db,date(2000,9,1), date(2012,9,30), 'recorded_on', 'snowfall')
     return dict(rows=rows.as_list())
 
 def forcasted():
